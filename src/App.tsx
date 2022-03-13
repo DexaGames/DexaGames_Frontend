95f01210df7333850a7c06c253fed6cba809c6ef
Lilianada <lilibest30.lb@gmail.com>
2022-03-13 23:22:32 +0100

    Header Implementation #4

--- src/App.tsx
+++ src/App.tsx
@@ -11,5 +11,7 @@ function App() {
     </div>
   );
 }
-
+require('react-dom');
+window.React2 = require('react');
+console.log(window.React1 === window.React2);
 export default App;
--- src/components/Header/HeaderSection.tsx
+++ src/components/Header/HeaderSection.tsx
@@ -1,11 +1,13 @@
-import "./HomeStyles.css";
+import 
+import "./HeaderStyles.css";
+import Hamburger from "./OffCanvasMenu";
 
 export default function Header () {
     return (
         <header>
-            <nav>
+            <nav className="navBar">
                 <h1 className="head">DexaGames</h1>
-                
+                <Hamburger />
             </nav>
         </header>
     )
