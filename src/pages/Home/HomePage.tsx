72b6d740a870c128405559e312b7fa3c8d278e14
Lilianada <lilibest30.lb@gmail.com>
2022-03-17 22:46:12 +0100

    HomePage responsiveness #9

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -19,6 +19,69 @@ export default function Home () {
                         <div className="gameWrapper">
                             <h2 className="headText">Pick the Game for you.</h2>
                             <p className="bodyText">Choose a game to play</p>
+
+                            <div className="gameBlocks">
+                                <div className="blockOne">
+                                    <div className="violet blockBar"></div>
+                                    <div className="blockContent">
+                                        <div>
+                                            <h3 className="heading">Numlock</h3>
+                                            <p>Puzzle</p>
+                                        </div>
+                                        <a href="/" className="nextIcon"> <MdNavigateNext fill="white"/> </a>
+                                    </div>
+                                </div>
+                                <div className="blockTwo">
+                                    <div className="orange blockBar"></div>
+                                    <div className="blockContent">
+                                        <div>
+                                            <h3 className="heading">Decko</h3>
+                                            <p>Puzzle</p>
+                                        </div>
+                                        <a href="/" className="nextIcon"> <MdNavigateNext fill="white"/> </a>
+                                    </div>
+                                </div>
+                                <div className="blockThree">
+                                    <div className="blue blockBar"></div>
+                                    <div className="blockContent">
+                                        <div>
+                                            <h3 className="heading">MatchBox</h3>
+                                            <p>Arcade</p>
+                                        </div>
+                                        <a href="/" className="nextIcon"> <MdNavigateNext fill="white"/> </a>
+                                    </div>
+                                </div>
+                                <div className="blockFour">
+                                    <div className="yellow blockBar"></div>
+                                    <div className="blockContent">
+                                        <div>
+                                            <h3 className="heading">Sliced</h3>
+                                            <p>Arcade</p>
+                                        </div>
+                                        <a href="/" className="nextIcon"> <MdNavigateNext fill="white"/> </a>
+                                    </div>
+                                </div>
+                                <div className="blockFive">
+                                    <div className="peach blockBar"></div>
+                                    <div className="blockContent">
+                                        <div>
+                                            <h3 className="heading">Sliced</h3>
+                                            <p>Arcade</p>
+                                        </div>
+                                        <a href="/" className="nextIcon"> <MdNavigateNext fill="white"/> </a>
+                                    </div>
+                                </div>
+                                <div className="blockSix">
+                                    <div className="red blockBar"></div>
+                                    <div className="blockContent">
+                                        <div>
+                                            <h3 className="heading">Sliced</h3>
+                                            <p>Arcade</p>
+                                        </div>
+                                        <a href="/" className="nextIcon"> <MdNavigateNext fill="white"/> </a>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                         <Footer /> 
                     </div>
