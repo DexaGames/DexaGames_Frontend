8e47c10bfae1f201c08b0354db234ed7164263eb
Lilianada <lilibest30.lb@gmail.com>
2022-04-06 02:01:13 +0100

    fixed clear and reset buttons on numlock page

--- src/pages/NumlockPage/Numlock.js
+++ src/pages/NumlockPage/Numlock.js
@@ -1,130 +1,68 @@
 import { useState } from "react";
 import "./NumlockStyles.css";
 import Wrapper from "./Wrapper";
-import Screen from "./ScreenDisplay";
+import {ScreenDisplay, Screen} from "./ScreenDisplay";
 import ButtonBox from "./ButtonWrap";
 import Button from "./Buttons";
 
-export function Numlocks () {
-    const [count, setCount] = useState();
-    const [countTwo, setCountTwo] = useState();
-    const [countThree, setCountThree] = useState();
-    const [countFour, setCountFour] = useState();
-
-    const handleReset = (e) => {
-        console.log(e)
-    }
-
-    return (
-        <div className="wrapper gameWrap">
-            <div className="headIntro">
-                <h1>Numlock</h1>
-                <h3>Let's get playing</h3>
-            </div>
-           
-                <div className="gameBoard">
-                    {/* <div className="boxTop"><h3>{count}</h3></div>
-                    <div className="boxTop"><h3>{countTwo}</h3></div>
-                    <div className="boxTop"><h3>{countThree}</h3></div> */}
-                    <input className="boxTop" type="" required placeholder="0"/>
-                    <input className="boxTop" type="" required placeholder="0"/>
-                    <input className="boxTop" type="" required placeholder="0"/>
-                    <input className="boxTop" type="" required placeholder="0"/>
-                </div>
-                <div className="gameKeyboard"> 
-                    <button onClick={e => setCount(1)} className="keyboard">
-                        <h3>1</h3>
-                    </button>
-                    <button onClick={e => setCountTwo(2)} className="keyboard">
-                        <h3>2</h3>
-                    </button>
-                    <button onClick={e => setCountThree(3)} className="keyboard">
-                        <h3>3</h3> 
-                    </button>
-                    <button onClick={e => setCountFour(4)}className="keyboard">
-                        <h3>4</h3>
-                    </button>
-                    <button className="keyboard">
-                        <h3>5</h3>
-                    </button>
-                    <button className="keyboard">
-                        <h3>6</h3>
-                    </button>
-                    <button className="keyboard">
-                        <h3>7</h3>
-                    </button>
-                    <button className="keyboard">
-                        <h3>8</h3>
-                    </button>
-                    <button className="keyboard">
-                        <h3>9</h3>
-                    </button>
-                    <button onClick={handleReset} className="keyboard red">
-                        <h3></h3>
-                    </button>
-                    <button className="keyboard">
-                        <h3>0</h3>
-                    </button>
-                    <button className="keyboard">
-                        <h3></h3>
-                    </button>
-                    <button className="spaceBar"><h3>clear</h3></button>
-                </div>
-                <div className="gameBoard">
-                    {/* <div className="boxBottom"><h3></h3></div>
-                    <div className="boxBottom"><h3></h3></div>
-                    <div className="boxBottom"><h3></h3></div>
-                    <div className="boxBottom"><h3></h3></div> */}
-                    <input className="boxBottom" type="" required placeholder="0"/>
-                    <input className="boxBottom" type="" required placeholder="0"/>
-                    <input className="boxBottom" type="" required placeholder="0"/>
-                    <input className="boxBottom" type="" required placeholder="0"/>
-                </div>
-            </div>
-        
-    )
-}
-
 const btnValues = [
-    [7, 8, 9],
-    [4, 5, 6],
     [1, 2, 3],
-    ["", 0, ""],
-    ["Clear"]
+    [4, 5, 6],
+    [7, 8, 9],
+    ["X", 0, "Reset"],
+    ['Enter']
 ];
 
 export default function Numlock () {
 
-    let [display, setDisplay] = useState({
-        num: 0,
-        res: 0,
-    });
+    let [display, setDisplay] = useState('');
 
     const numClickHandler = (e) => {
         e.preventDefault();
         const value = e.target.innerHTML;
-    
-        if (display.num.length < 2) {
-          setDisplay({
-            ...display,
-            num:
-              display.num === 0 && value === "0"
-                ? "0"
-                : display.num % 1 === 0
-                ? Number(display.num + value)
-                : display.num + value,
-            res: !display.sign ? 0 : display.res,
-          });
+        
+        if (value == 'X') {
+            backspaceClickHandler()
+            return
+        }
+
+        if (display.length < 4) {
+         setDisplay(
+                display + value
+            );
         }
     };
 
     const resetClickHandler = () => {
-        setDisplay({
-          ...display,
-          num: 0,
-          res: 0,
-        });
+        setDisplay(display = '');
     };
+
+    const backspaceClickHandler = () => {
+        const show = display.split('')
+        show.length = show.length - 1
+        setDisplay(show.join(''))
+    }
+
+    const getClassName = (btn) => {
+        if (btn == "Enter") {
+            return "Enter"
+        }else if (btn == "Reset") {
+            return "Reset"
+        }else if (btn == "X"){
+            return "clear"
+        }
+        else {return ""}
+    }
+
+    const handleEnter = (btn) => {
+        if (btn === "Reset" ){
+            return resetClickHandler 
+        }if (btn === "Enter") {
+            return 
+        }else {return numClickHandler}
+
+    }
+    
 
     return (
         <div className="wrapper gameWrap">
@@ -133,23 +71,22 @@ export default function Numlock () {
                 <h3>Let's get playing</h3>
             </div>
             <Wrapper>
-                <Screen value={display.num ? display.num : display.res} />
+                    <ScreenDisplay value={display} />
                 <ButtonBox>
                     {
                     btnValues.flat().map((btn, i) => {
                         return (
                         <Button
                             key={i}
-                            className={btn === "Clear" ? "c" : ""}
+                            className={getClassName (btn)}
                             value={btn}
-                            onClick={
-                                btn === "Clear" ? resetClickHandler : numClickHandler
-                            }
+                            onClick={handleEnter(btn)}
                         />
                         );
                     })
                     }
                 </ButtonBox>
+                <Screen value={display} />
             </Wrapper>
         </div>
     )
--- src/pages/NumlockPage/ScreenDisplay.js
+++ src/pages/NumlockPage/ScreenDisplay.js
@@ -1,12 +1,48 @@
 import { Textfit } from "react-textfit";
 import "./NumlockStyles.css";
 
-const ScreenDisplay = ({ value }) => {
+export const ScreenDisplay = ({ value }) => {
+    const show = value.toString().split("")
+    
   return (
-    <Textfit className="screen" mode="single" max={4}>
-      {value}
-    </Textfit>
+    <div className="screenWrap">
+        <Textfit className="screen" mode="single" max={24}>
+            {show.length > 0 ? show[0] : ""}
+            
+        </Textfit>
+        <Textfit className="screen" mode="single" max={24}>
+            {show.length > 1 ? show[1] : ""}
+        </Textfit>
+        <Textfit className="screen" mode="single" max={24}>
+            {show.length > 2 ? show[2] : ""}
+        </Textfit>
+        <Textfit className="screen" mode="single" max={24}>
+            {show.length > 3 ? show[3] : ""}
+        </Textfit>
+    </div>
   );
 };
 
-export default ScreenDisplay;
+
+export const Screen = ({ value }) => {
+    const show = value.toString().split("")
+    
+  return (
+    <div className="screenWrap">
+        <Textfit className="screen" mode="single" max={24}>
+            {show.length > 0 ? show[0] : ""}
+            
+        </Textfit>
+        <Textfit className="screen" mode="single" max={24}>
+            {show.length > 1 ? show[1] : ""}
+        </Textfit>
+        <Textfit className="screen" mode="single" max={24}>
+            {show.length > 2 ? show[2] : ""}
+        </Textfit>
+        <Textfit className="screen" mode="single" max={24}>
+            {show.length > 3 ? show[3] : ""}
+        </Textfit>
+    </div>
+  );
+};
+
